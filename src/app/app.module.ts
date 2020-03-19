c516a3d178cbbe4f22a888308eaa89461c227bec
pablo m <pablomf89@gmail.com>
2020-03-19 20:50:32 +0100

    Add global elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { GlobalElementsComponent } from './global-elements/global-elements.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, GlobalElementsComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/global-elements/global-elements.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-global-elements',
+  templateUrl: './global-elements.component.html',
+  styleUrls: ['./global-elements.component.scss']
+})
+export class GlobalElementsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
