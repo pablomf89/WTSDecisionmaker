89881b0995250a2f683798630df2b2e40be1dfb5
pablo m <pablomf89@gmail.com>
2020-03-19 20:44:18 +0100

    add  header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { MasterComponent } from "./master.component";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
-  declarations: [AppComponent, MasterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/master.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from "@angular/core";
-@Component({
-  selector: "master",
-  template: "<h2>Master</h2>"
-})
-export class MasterComponent {}
