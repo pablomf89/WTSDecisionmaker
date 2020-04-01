d64705cd8eed5d584971b4226da4253f1882c1ab
javiermartinust <javier.martin1@ust-global.com>
2020-04-01 14:54:56 +0200

    added button for switching themes in user header expandable card

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, Inject } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { DOCUMENT } from '@angular/common';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  constructor(public overlayContainer: OverlayContainer, @Inject(DOCUMENT) private document: Document) { }
+  @HostBinding('class') componentCssClass;
 
-  constructor() { }
 
   ngOnInit(): void {
   }
-
+  onSetTheme(previousTheme, newTheme) {
+    if (newTheme != '' && !this.document.body.classList.contains(newTheme)) {
+      this.document.body.classList.add(newTheme);
+    }
+    if (previousTheme != '' && this.document.body.classList.contains(previousTheme)) {
+      this.document.body.classList.remove(previousTheme);
+    }
+    this.componentCssClass = newTheme;
+  }
 }
