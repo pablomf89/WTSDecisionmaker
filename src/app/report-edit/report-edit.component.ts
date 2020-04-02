f0bc8da6288f1eb58ef06e2883118ce5006e9929
javiermartinust <javier.martin1@ust-global.com>
2020-04-02 14:46:33 +0200

    added edit report component

---
+++ src/app/report-edit/report-edit.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+import {
+} from "@angular/forms";
+
+@Component({
+  selector: "app-report-edit",
+  templateUrl: "./report-edit.component.html",
+  styleUrls: ["./report-edit.component.scss"]
+})
+
+export class ReportEditComponent implements OnInit {
+
+  constructor() { }
+
+  async ngOnInit() {
+  }
+
+
+}
