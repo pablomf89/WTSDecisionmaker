24ddacde67d53f2e4c1b37fce64e265f5bf25472
javiermartinust <javier.martin1@ust-global.com>
2020-04-02 13:35:54 +0200

    added edit decisions template

---
+++ src/app/decision-edit/decision-edit.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+import {
+} from "@angular/forms";
+
+@Component({
+  selector: "app-decision-edit",
+  templateUrl: "./decision-edit.component.html",
+  styleUrls: ["./decision-edit.component.scss"]
+})
+
+export class DecisionEditComponent implements OnInit {
+
+  constructor() { }
+
+  async ngOnInit() {
+  }
+
+
+}
