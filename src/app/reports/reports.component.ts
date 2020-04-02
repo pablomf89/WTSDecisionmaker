3604bf4ed522b7061a27025fb5ff5a15f609d81f
javiermartinust <javier.martin1@ust-global.com>
2020-04-02 14:46:12 +0200

    added reports list component

---
+++ src/app/reports/reports.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+import {
+} from "@angular/forms";
+
+@Component({
+  selector: "app-decisions",
+  templateUrl: "./reports.component.html",
+  styleUrls: ["./reports.component.scss"]
+})
+
+export class ReportsComponent implements OnInit {
+
+  constructor() { }
+
+  async ngOnInit() {
+  }
+
+}
