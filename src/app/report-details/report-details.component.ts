44b311bd084a94ec7db4d4587238ac91865a22cd
javiermartinust <javier.martin1@ust-global.com>
2020-04-02 14:46:39 +0200

    added report details component

---
+++ src/app/report-details/report-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+import {
+} from "@angular/forms";
+
+@Component({
+  selector: "app-report-details",
+  templateUrl: "./report-details.component.html",
+  styleUrls: ["./report-details.component.scss"]
+})
+
+export class ReportDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  async ngOnInit() {
+  }
+
+
+}
